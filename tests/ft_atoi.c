69875f5e05f438054442245a55bc79ee71a157b0
yfe404 <yfe.github@protonmail.com>
2025-03-09 14:00:24 +0100

    more tests for atoi including INT_MAX / INT_MIN

--- tests/ft_atoi.c
+++ tests/ft_atoi.c
@@ -9,6 +9,11 @@ int main() {
     assert(ft_atoi("-42") == -42);
     assert(ft_atoi("42") == 42);
     assert(ft_atoi(NULL) == 0);
+    assert(ft_atoi("9999") == 9999);
+    assert(ft_atoi("2147483647") == 2147483647);
+    assert(ft_atoi("-2147483648") == -2147483648);
+    assert(ft_atoi("         -2147483648") == -2147483648);
+    assert(ft_atoi("         +2147483647") == 2147483647);
 
     printf("All tests passed!\n");
     return 0;
