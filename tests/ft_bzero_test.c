b915861b73d37678b46b068f6b88802a08d37729
yfe404 <yfe.github@protonmail.com>
2025-03-16 10:41:46 +0100

    more test, close #1 and #2

--- tests/ft_bzero_test.c
+++ tests/ft_bzero_test.c
@@ -6,6 +6,8 @@ void	ft_bzero(void *s, size_t n);
 
 int main() {
     char buffer[] = "Hello friend!"; 
+    char buffer2[] = "\0Hello friend!"; 
+
 	assert(strlen(buffer) == 13);
 	ft_bzero(buffer+5, 1);
 	assert(strlen(buffer) == 5);
@@ -14,6 +16,12 @@ int main() {
 	assert(buffer[4] == 'o');
 	ft_bzero(buffer+3, 3);
 	assert(buffer[4] == '\0');
+
+	ft_bzero(buffer2, 5);
+	assert(buffer2[1] == '\0');
+	assert(buffer2[2] == '\0');
+	assert(buffer2[3] == '\0');
+	assert(buffer2[4] == '\0');
 
     printf("All tests passed!\n");
     return 0;
