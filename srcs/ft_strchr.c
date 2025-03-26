2f7614479cd17d44a6a709b360033cbdf0d06888
yfe404 <yfe.github@protonmail.com>
2025-03-26 15:28:20 +0100

    fix: edge case when param c is bigger than 127

--- srcs/ft_strchr.c
+++ srcs/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: yfeunteu <yfeunteu@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 13:04:52 by yfeunteu          #+#    #+#             */
-/*   Updated: 2025/03/25 13:04:53 by yfeunteu         ###   ########.fr       */
+/*   Updated: 2025/03/26 15:27:31 by yfeunteu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ char	*ft_strchr(const char *s, int c)
 
 	pos = 0;
 	len = ft_strlen(s);
+	c %= 128;
 	while (s && (pos <= len))
 	{
 		if (s[pos] == c)
