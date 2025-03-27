19cf4ef9a30a853aec05c76680e7c301195b294b
yfe404 <yfe.github@protonmail.com>
2025-03-27 17:56:10 +0100

    update according to newestr subject

--- srcs/ft_lstnew.c
+++ srcs/ft_lstnew.c
@@ -6,39 +6,21 @@
 /*   By: yfeunteu <yfeunteu@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 19:05:21 by yfeunteu          #+#    #+#             */
-/*   Updated: 2025/03/25 19:05:23 by yfeunteu         ###   ########.fr       */
+/*   Updated: 2025/03/27 10:00:02 by yfeunteu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t size);
-
-t_list	*ft_lstnew(void const *content, size_t content_size)
+t_list	*ft_lstnew(void *content)
 {
 	t_list	*link;
 
-	link = (t_list *)malloc(sizeof(t_list *));
+	link = (t_list *)malloc(sizeof(t_list));
 	if (!link)
 		return (NULL);
-	if (!content)
-	{
-		link->content_size = 0;
-		link->content = NULL;
-		link->next = NULL;
-	}
-	else
-	{
-		link->content_size = content_size;
-		link->content = malloc(content_size);
-		if (!link->content)
-		{
-			free (link);
-			return (NULL);
-		}
-		link->content = ft_memcpy(link->content, content, content_size);
-		link->next = NULL;
-	}
+	link->content = content;
+	link->next = NULL;
 	return (link);
 }
