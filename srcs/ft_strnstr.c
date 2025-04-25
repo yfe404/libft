c7338efd733580fd6ba17ca161b2e4696ab9d8d4
yfe404 <yfe.github@protonmail.com>
2025-04-25 13:02:22 +0200

    fix: sign compare warning

--- srcs/ft_strnstr.c
+++ srcs/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: yfeunteu <yfeunteu@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 14:05:59 by yfeunteu          #+#    #+#             */
-/*   Updated: 2025/03/26 13:06:06 by yfeunteu         ###   ########.fr       */
+/*   Updated: 2025/03/27 23:15:42 by yfeunteu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	if (needle_len == 0)
 		return ((char *)haystack);
 	ret = ft_strchr(haystack, needle[0]);
-	while (ret && ((ret - haystack) <= (len - needle_len)))
+	while (ret && haystack && ((size_t)(ret - haystack) <= (len - needle_len)))
 	{
 		if (ft_strnequ(ret, needle, needle_len))
 			return (ret);
