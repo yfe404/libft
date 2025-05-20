62e53162c611749fcdf46c7fdc575e104c2e4f5b
yfe404 <yfe.github@protonmail.com>
2025-05-20 14:14:50 +0200

    fix: undeclared function

--- srcs/ft_strtrim.c
+++ srcs/ft_strtrim.c
@@ -6,14 +6,17 @@
 /*   By: yfeunteu <yfeunteu@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 21:18:16 by yfeunteu          #+#    #+#             */
-/*   Updated: 2025/03/26 16:07:22 by yfeunteu         ###   ########.fr       */
+/*   Updated: 2025/05/20 14:07:37 by yfeunteu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
+#include <stdlib.h>
 
 size_t	ft_strlen(const char *s);
-char	*ft_strsub(char const *s, unsigned int start, size_t len);
+char 	*ft_strdup(const char *s);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	ft_bzero(void *ptr, size_t size);
 
 static char	ft_iswhitespace(char c, const char *set)
 {
@@ -21,6 +24,16 @@ static char	ft_iswhitespace(char c, const char *set)
 		if (*set++ == c)
 			return (1);
 	return (0);
+}
+
+static char	*ft_strnew(size_t size)
+{
+	char	*ptr;	
+
+	ptr = (char *)malloc(size);
+	if (ptr != NULL)
+		ft_bzero(ptr, size);
+	return (ptr);
 }
 
 char	*ft_strtrim(char const *s, const char *set)
@@ -45,6 +58,10 @@ char	*ft_strtrim(char const *s, const char *set)
 	len = pos_end - pos_start + 1;
 	if (pos_end < pos_start)
 		len = 0;
-	ret = ft_strsub(s, pos_start, len);
+	
+	ret = ft_strnew(len + 1);
+	ret = ft_memcpy(ret, s + pos_start, len);
+	ret[len] = '\0';
+
 	return (ret);
 }
