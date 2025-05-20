8107437aa38f5b69e5bee40248229761a50751e0
yfe404 <yfe.github@protonmail.com>
2025-05-20 15:26:42 +0200

    fix: missing func decl

--- srcs/ft_itoa.c
+++ srcs/ft_itoa.c
@@ -6,20 +6,35 @@
 /*   By: yfeunteu <yfeunteu@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 21:47:09 by yfeunteu          #+#    #+#             */
-/*   Updated: 2025/03/24 22:17:11 by yfeunteu         ###   ########.fr       */
+/*   Updated: 2025/05/20 15:02:09 by yfeunteu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
+#include <stdlib.h>
 
 char	*ft_strsub(char const *s, unsigned int start, size_t len);
 void	*ft_memset(void *s, int c, size_t n);
+void	ft_bzero(void *ptr, size_t size);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+
+static char	*ft_strnew(size_t size)
+{
+	char	*ptr;	
+
+	ptr = (char *)malloc(size);
+	if (ptr != NULL)
+		ft_bzero(ptr, size);
+	return (ptr);
+}
 
 char	*ft_itoa(int n)
 {
 	int				sign;
 	char			temp[12];
 	unsigned int	pos;
+	int				buffsize;
+	char			*ret;
 
 	sign = n < 0;
 	pos = 10;
@@ -34,5 +49,9 @@ char	*ft_itoa(int n)
 	}
 	if (sign)
 		temp[pos--] = '-';
-	return (ft_strsub(temp, pos + 1, 10 - pos));
+	buffsize = 10 - pos;
+	ret = ft_strnew(buffsize + 1);
+	ret = ft_memcpy(ret, &temp[pos + 1], buffsize);
+	ret[buffsize] = '\0';
+	return (ret);
 }
