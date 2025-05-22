018a1086cea8820d7ea127de61cbe516a3aaef14
yfe404 <yfe.github@protonmail.com>
2025-05-22 23:31:27 +0200

    fixL: remove errno

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,12 +6,11 @@
 /*   By: yfeunteu <yfeunteu@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 19:51:06 by yfeunteu          #+#    #+#             */
-/*   Updated: 2025/03/26 22:39:34 by yfeunteu         ###   ########.fr       */
+/*   Updated: 2025/05/22 23:28:41 by yfeunteu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <errno.h>
 #include <stdint.h>
 
 void	ft_bzero(void *s, size_t n);
@@ -21,16 +20,10 @@ void	*ft_calloc(size_t n, size_t size)
 	void	*ptr;
 
 	if (n > 0 && size > SIZE_MAX / n)
-	{
-		errno = ENOMEM;
 		return (NULL);
-	}
 	ptr = malloc(n * size);
 	if (!ptr)
-	{
-		errno = ENOMEM;
 		return (NULL);
-	}
 	ft_bzero(ptr, n * size);
 	return (ptr);
 }
--- ft_strdup.c
+++ ft_strdup.c
@@ -6,12 +6,11 @@
 /*   By: yfeunteu <yfeunteu@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/15 11:44:03 by yfeunteu          #+#    #+#             */
-/*   Updated: 2025/03/15 11:44:04 by yfeunteu         ###   ########.fr       */
+/*   Updated: 2025/05/22 23:28:04 by yfeunteu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <errno.h>
 
 size_t	ft_strlen(const char *s);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
@@ -24,10 +23,7 @@ char	*ft_strdup(const char *s)
 	len = ft_strlen(s);
 	new = malloc(len + 1);
 	if (new == NULL)
-	{
-		errno = ENOMEM;
 		return (NULL);
-	}
 	ft_memcpy(new, s, len + 1);
 	return (new);
 }
