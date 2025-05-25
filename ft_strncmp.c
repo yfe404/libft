6075eb868537ccb18b72922ae404c8ab0a5eabb9
yfe404 <yfe.github@protonmail.com>
2025-05-25 19:25:15 +0200

    fix:  break for characters with values > 127

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: yfeunteu <yfeunteu@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 11:06:01 by yfeunteu          #+#    #+#             */
-/*   Updated: 2025/03/17 13:08:56 by yfeunteu         ###   ########.fr       */
+/*   Updated: 2025/05/25 19:23:01 by yfeunteu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,5 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 		s1++;
 		s2++;
 	}
-	return (*s1 - *s2);
+	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
